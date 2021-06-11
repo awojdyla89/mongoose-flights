2b68d802e94d0699524a7178b0af36d5ac324da1
Adam Wojdyla <adam@Adams-MacBook-Pro.local>
2021-06-11 09:31:39 -0600

    added default values for Flight property select tags

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,14 +8,9 @@ module.exports = {
 
 function create(req, res){
     
-    if(req.body.airline == ''){
-        req.body.airline == 'n/a';
-    }
-    if(req.body.airport.value == ''){
-        req.body.airport.cast == 'DEN';
-    }
+    
     if(req.body.flightNo < 10 && req.body.flightNo > 9999){
-        req.body.flightNo.cast == 'n/a';
+        req.body.flightNo == 'n/a';
     }
     if(req.body.departs == null){
         req.body.departs.cast == 'n/a';
@@ -24,8 +19,13 @@ function create(req, res){
         console.log(req.body.airport);
         console.log(req.body.flightNo);
         console.log(req.body.departs);
-    // Flight.create(req.body, function(err, flightDocument){
-    // })
+
+     Flight.create(req.body, function(err, flightDocument){
+         if(err) {
+             console.log(err);
+         return res.render('flights/new')
+         }
+     })
     
     res.redirect('/flights')
 }
